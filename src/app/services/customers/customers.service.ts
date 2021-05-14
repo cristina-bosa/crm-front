f78e907cd3e297c9cf7645827760d9a13fd3a54a
Cristina Bosa <cristinabosasanchez@gmail.com>
2021-05-14 20:10:48 +0100

    change customers service

--- src/app/services/customers/customers.service.ts
+++ src/app/services/customers/customers.service.ts
@@ -1,4 +1,4 @@
-import { HostListener, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import axios from 'axios';
 import { Customer } from 'src/app/models/customer.model';
 
@@ -6,11 +6,12 @@ import { Customer } from 'src/app/models/customer.model';
   providedIn: 'root'
 })
 export class CustomersService {
-
+  apiURL = "http://localhost:3000/customers";
+  
   constructor() { }
 
   getAll() : Promise<Customer[]>{
-    return axios.get('https://jsonplaceholder.typicode.com/users/')
+    return axios.get(this.apiURL)
     .then(res => res.data)
     .catch(e => console.log(e));
   }
